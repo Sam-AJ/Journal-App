c33a4f2ba9d60c30c687c3de96d3e72d3bc366df
Samuel Jara <spjara.aravena@gmail.com>
2022-08-10 17:52:01 -0400

    Test en auth/thunks completado

--- test/store/auth/thunks.test.js
+++ test/store/auth/thunks.test.js
@@ -1,6 +1,7 @@
-import { signInWithGoogle } from '../../../src/firebase';
-import { checkingCredentials } from '../../../src/store/auth';
-import { checkingAuthentication, startGoogleSignIn } from '../../../src/store/auth/thunks';
+import { loginWithEmailPassword, logoutFirebase, signInWithGoogle } from '../../../src/firebase';
+import { checkingCredentials, login, logout } from '../../../src/store/auth';
+import { checkingAuthentication, startGoogleSignIn, startLoginWithEmailPassword, startLogout } from '../../../src/store/auth/thunks';
+import { clearNotesLogout } from '../../../src/store/journal/journalSlice';
 import { demoUser } from '../../fixtures/authFixtures';
 
 jest.mock('../../../src/firebase/providers');
@@ -23,5 +24,41 @@ describe('Pruebas en AuthThunks', () => {
         await signInWithGoogle.mockResolvedValue(loginData);
 
         await startGoogleSignIn()(dispatch);
+
+        expect(dispatch).toHaveBeenCalledWith(checkingCredentials());
+        expect(dispatch).toHaveBeenCalledWith(login(loginData));
+    });
+
+    test('startGoogleSignIn debe llamar checkingCredentials y logout - Error', async () => {
+
+        const loginData = { ok: false, errorMessage: 'Un error en Google' };
+        await signInWithGoogle.mockResolvedValue(loginData);
+
+        await startGoogleSignIn()(dispatch);
+
+        expect(dispatch).toHaveBeenCalledWith(checkingCredentials());
+        expect(dispatch).toHaveBeenCalledWith(logout(loginData.errorMessage));
+    });
+
+    test('startLoginWithEmailPassword debe llamar checkingCredentials y login - Éxito', async () => {
+
+        const loginData = { ok: true, ...demoUser };
+        const formData = { email: demoUser.email, password: '123456' };
+
+        await loginWithEmailPassword.mockResolvedValue(loginData);
+
+        await startLoginWithEmailPassword(formData)(dispatch);
+
+        expect(dispatch).toHaveBeenCalledWith(checkingCredentials());
+        expect(dispatch).toHaveBeenCalledWith(login(loginData));
+    });
+
+    test('startLogout debe llamar logoutFirebase, clearNotes y logout', async() => {
+
+        await startLogout()(dispatch);
+
+        expect(logoutFirebase).toHaveBeenCalled();
+        expect(dispatch).toHaveBeenCalledWith(clearNotesLogout());
+        expect(dispatch).toHaveBeenCalledWith(logout());
     });
 });
